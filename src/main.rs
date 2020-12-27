728133bb0ff5a9f4cf2a97233e3c129c0807c67c
Koji Ota <afterjnih@gmail.com>
2020-12-27 21:54:46 +0900

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use my_dropbox_controller::digest::{dpx_digest, sha_256_digest};
 use my_dropbox_controller::dropbox::{get_file_metadata, list_directory, upload_file};
 use my_dropbox_controller::extension::Extension;
 use my_dropbox_controller::meta::{get_datetime, get_mp4_datetime};
-use my_dropbox_controller::sqlite::reset_db;
+use my_dropbox_controller::sqlite::reset_db as sqlite_reset_db;
 use std::fmt;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Read, Seek, SeekFrom};
@@ -49,56 +49,69 @@ enum MyError {
     InvalidExtensionString(String),
 }
 
+async fn reset_db() -> Result<()> {
+    println!("resetDB");
+    println!("{:?}", sqlite_reset_db("my-dropbox.db3").await);
+    let mut source_file = File::open("my-dropbox.db3")?;
+    upload_file(source_file, "/my-dropbox2.db3".to_string())?;
+    Ok(())
+}
+
+fn upload(path: &Path) {
+    println!("upload");
+}
+fn get_metadata(path: &Path) -> Result<()> {
+    println!("meta");
+    let ext = Extension::from_str(
+        path.extension()
+            .ok_or(MyError::InvalidPathError("invalid path".to_string()))
+            .and_then(|ext| {
+                ext.to_str()
+                    .ok_or(MyError::InvalidExtensionString(format!("{:?}", path)))
+            })?,
+    )?;
+    let mut file =
+        File::open(&path).with_context(|| format!("failed to open file: {:?}", path.to_str()))?;
+    let mut buff = BufReader::new(&file);
+    let digest = sha_256_digest(&mut buff);
+    // sha_256_digest2(&file);
+    match ext {
+        Extension::Jpeg => {
+            println!("pic");
+            println!("{:?}", get_datetime(&mut buff));
+            // get_file_metadata(&format!(
+            //     "/ファミリー ルーム/写真/{}",
+            //     path.to_str().unwrap()
+            // ));
+        }
+        Extension::Mp4 => {
+            println!("mov");
+            println!("{:?}", get_mp4_datetime(&mut buff));
+            buff.seek(SeekFrom::Start(0))?;
+            // get_file_metadata(&format!(
+            //     "/ファミリー ルーム/動画/{}",
+            //     path.to_str().unwrap()
+            // ));
+        }
+        _ => {}
+    }
+    println!("digest: {:?}", HEXUPPER.encode(digest.unwrap().as_ref()));
+    println!("dpx_digest: {:?}", dpx_digest(&mut buff));
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = Cli::from_args();
     match args.sub {
         Sub::ResetDb => {
-            println!("resetDB");
-            println!("{:?}", reset_db("my-dropbox.db3").await);
-            let mut source_file = File::open("my-dropbox.db3")?;
-            upload_file(source_file, "/my-dropbox2.db3".to_string())?;
+            reset_db();
         }
         Sub::Upload { path } => {
-            println!("upload");
+            upload(&path);
         }
         Sub::Meta { path } => {
-            println!("meta");
-            let ext = Extension::from_str(
-                path.extension()
-                    .ok_or(MyError::InvalidPathError("invalid path".to_string()))
-                    .and_then(|ext| {
-                        ext.to_str()
-                            .ok_or(MyError::InvalidExtensionString(format!("{:?}", path)))
-                    })?,
-            )?;
-            let mut file = File::open(&path)
-                .with_context(|| format!("failed to open file: {:?}", path.to_str()))?;
-            let mut buff = BufReader::new(&file);
-            let digest = sha_256_digest(&mut buff);
-            // sha_256_digest2(&file);
-            match ext {
-                Extension::Jpeg => {
-                    println!("pic");
-                    println!("{:?}", get_datetime(&mut buff));
-                    // get_file_metadata(&format!(
-                    //     "/ファミリー ルーム/写真/{}",
-                    //     path.to_str().unwrap()
-                    // ));
-                }
-                Extension::Mp4 => {
-                    println!("mov");
-                    println!("{:?}", get_mp4_datetime(&mut buff));
-                    buff.seek(SeekFrom::Start(0))?;
-                    // get_file_metadata(&format!(
-                    //     "/ファミリー ルーム/動画/{}",
-                    //     path.to_str().unwrap()
-                    // ));
-                }
-                _ => {}
-            }
-            println!("digest: {:?}", HEXUPPER.encode(digest.unwrap().as_ref()));
-            println!("dpx_digest: {:?}", dpx_digest(&mut buff));
+            get_metadata(&path);
         }
         Sub::Test { path } => {
             println!("test");
